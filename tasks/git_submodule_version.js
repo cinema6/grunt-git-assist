557c243a67875f4c713b32051d93eb6fc38915fd
Howard Engelhart <howardengelhart@yahoo.com>
2013-11-08 22:06:58 -0500

    actually added the module

---
+++ tasks/git_submodule_version.js
@@ -0,0 +1,41 @@
+/*
+ * grunt-git-assist
+ * 
+ *
+ * Copyright (c) 2013 howard
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path=require('path');
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask('submodule_version','Gets current versions of modules.',function(){
+        var target = this.target,
+            data   = this.data,
+            args   = [],
+            done   = this.async();
+
+        if (!data.path){
+            grunt.log.errorlns('submodule_add:' + target + ' - fails, no path');
+            return done(false);
+        }
+
+
+        grunt.util.spawn({
+            cmd : 'git',
+            args : ['describe','--tags','--long'],
+            opts : { cwd : data.path }
+        },function(error/*,result,code*/){
+            if (error) {
+                grunt.log.errorlns('submodule_version:' + target + ' - failed: ' + error);
+                done(false);
+                return;
+            }
+
+            grunt.log.writelns('submodule_version:' + target + ' = ' + result);
+        });
+    });
+};
